9d98916ec84f32820f1d823d6e5997c7c748518f
Clara Ferreira Batista <ferreirabatistaclara@gmail.com>
2023-03-28 22:46:22 -0300

    update

--- src/database/database.sql
+++ src/database/database.sql
@@ -1,23 +1,24 @@
+
 CREATE TABLE users (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     name TEXT NOT NULL,
-		email TEXT UNIQUE NOT NULL,
-		password TEXT NOT NULL
+    email TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL
 );
 
 CREATE TABLE tasks (
     id TEXT PRIMARY KEY UNIQUE NOT NULL,
     title TEXT NOT NULL,
-		description TEXT NOT NULL,
-		created_at TEXT DEFAULT (DATETIME()) NOT NULL,
-		status INTEGER DEFAULT (0) NOT NULL
+    description TEXT NOT NULL,
+    created_at TEXT DEFAULT (DATETIME()) NOT NULL,
+    status INTEGER DEFAULT (0) NOT NULL
 );
 
 CREATE TABLE users_tasks (
-		user_id TEXT NOT NULL,
-		task_id TEXT NOT NULL,
-		FOREIGN KEY (user_id) REFERENCES users (id),
-		FOREIGN KEY (task_id) REFERENCES tasks (id)
+    user_id TEXT NOT NULL,
+    task_id TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    FOREIGN KEY (task_id) REFERENCES tasks (id) ON UPDATE CASCADE
 );
 
 INSERT INTO users (id, name, email, password)
@@ -42,3 +43,15 @@ VALUES
 SELECT * FROM users;
 SELECT * FROM tasks;
 SELECT * FROM users_tasks;
+
+SELECT * FROM users_tasks
+INNER JOIN users
+ON users_tasks.user_id = users.id
+RIGHT JOIN tasks
+ON users_tasks.task_id = tasks.id;
+
+SELECT * FROM tasks
+LEFT JOIN users_tasks
+ON users_tasks.task_id = tasks.id
+LEFT JOIN users
+ON users_tasks.user_id = users.id;
